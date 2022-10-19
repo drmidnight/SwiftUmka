1000ea45231a399b2869074b9ad9735008852d6b
drmidnight <drmidnight@gmail.com>
2022-10-19 12:32:44 -0700

    removed submodule files

--- Sources/CUmka/umka-lang/tests/lib/build_lib_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-gcc -fPIC -O3 -c lib.c
-gcc -shared -fPIC -static-libgcc *.o -o lib.umi
-rm -f *.o
--- Sources/CUmka/umka-lang/tests/lib/build_lib_windows_mingw.bat
+++
@@ -1,3 +0,0 @@
-gcc -O3 -c lib.c
-gcc -shared -Wl,--dll *.o -o lib.umi -static-libgcc -static  
-del *.o
