206256fcb84bbdf702097ffae3fec093c1924fbb
drmidnight <drmidnight@gmail.com>
2022-10-19 14:18:21 -0700

    minor cleanup

--- Tests/UmkaTests/UmkaTests.swift
+++ Tests/UmkaTests/UmkaTests.swift
@@ -17,7 +17,6 @@ final class UmkaTests: XCTestCase {
 
         type (
             Vector2 = struct {x, y: real32}
-            Vector3 = struct {x, y, z: real32}
             Color   = struct {r, g, b, a: uint8}
         )
 
@@ -26,7 +25,7 @@ final class UmkaTests: XCTestCase {
         fn testColorStruct(color: Color)
         fn testVec2Struct(v: Vector2)
 
-        fn testCall() {
+        fn main() {
             printFunc("Printing")
             testMultiParam("testMulti", 32)
             testColorStruct(Color{ 90,  90,  90, 255})
@@ -34,19 +33,16 @@ final class UmkaTests: XCTestCase {
             std.println("Test standard lib")
         }
 
-        fn main() {
-            
-        }
-
         """
+        
         let umka = Umka(sourceString: source, stackSize: 1024*1024, fileSystemEnabled: true, implLibsEnabled: true)
         defer { umka.free() }
+        
         umka.addFunc(name: "printFunc") { slot, val in
             guard let message: String  = slot?.pointee.ptrAsString() else { return }
             print(message)
         }
         
-        // doesn't work. Buffer ptr is 8bit chunks and need to load 32bit slices
         umka.addFunc(name: "testVec2Struct") { slot, val in
             guard let vec: Vector2 = slot?.pointee.ptrAsType() else { return }
             print(vec)
@@ -68,7 +64,7 @@ final class UmkaTests: XCTestCase {
             print(umka.getError())
         }
         
-        let funcInt = umka.getFunc(funcName: "testCall")
+        let funcInt = umka.getFunc(funcName: "main")
         let pass = umka.call(funcInt)
         if pass == false { 
             print(umka.getError())
